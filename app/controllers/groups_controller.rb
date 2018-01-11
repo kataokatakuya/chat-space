4baa1720752900cbff53d688297b2d68246ccd31
kataokatakuya <kataokatakuya>
2018-01-11 12:58:45 +0900

    Display side_bar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,15 @@
 class GroupsController < ApplicationController
 
+def index
+end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
 
   def create
-    @group = Group.new
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
     else
@@ -19,3 +22,4 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
+
