a76542c967b7475ec7452fd873e712c421255cbb
kataokatakuya <kataokatakuya>
2018-01-13 20:20:53 +0900

    Correct code

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+  def date_time
+    created_at.in_time_zone('Tokyo').strftime('%Y/%m/%d %H:%M:%S')
+  end
+
 end
