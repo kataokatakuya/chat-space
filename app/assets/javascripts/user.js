7e46d02f7a0127be641f067bd680fadd77200319
kataokatakuya <kataokatakuya>
2018-01-20 15:19:12 +0900

    Correcting after advice

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,27 +1,30 @@
 $(function() {
 
+  var search_list = $("#user-search-result");
+  var chat_member_list = $("#chat-group-users");
+
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                 </div>`
-    $("#user-search-result").append(html);
+    search_list.append(html);
   }
 
   function appendNoUser(user) {
     var html = `<div class = "chat-group-user clearfix">
                   <p class = 'user_search_result_list'>${user}</p>
                 </div>`
-    $("#user-search-result").append(html);
+    search_list.append(html);
   }
 
-  function appendAdd(user_name, user_id) {
+  function appendAddUser(user_name, user_id) {
     var html = `<div class= 'chat-group-user clearfix js-chat-member' id = 'chat-group-user-1'>
                   <input name="group[user_ids][]", type='hidden', value="${ user_id}">
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${ user_id }" data-user-name="${ user_name}">削除</a>
                 </div>`
-    $("#chat-group-users").append(html);
+    chat_member_list.append(html);
   }
 
   $(".chat-group-form__input.search").on("keyup", function() {
@@ -35,7 +38,7 @@ $(function() {
         dataType: 'json'
       })
       .done(function(users) {
-        $("#user-search-result").empty();
+        search_list.empty();
         if (users.length !== 0) {
           users.forEach(function(user){
             appendUser(user);
@@ -50,13 +53,13 @@ $(function() {
       });
     }
   });
-  $("#user-search-result").on('click', ".user-search-add", function() {
+  search_list.on('click', ".user-search-add", function() {
     $(this).parent().remove();
     var user_name = $(this).data("user-name")
     var user_id = $(this).data("user-id")
-    appendAdd(user_name, user_id);
+    appendAddUser(user_name, user_id);
   });
-  $("#chat-group-users").on('click', ".user-search-remove", function() {
+  chat_member_list.on('click', ".user-search-remove", function() {
     $(this).parent().remove();
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
+  resources :groups, except: [:show, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
